b4ff229d993dd1c094aa7a86b9d8b0bbe39808d6
Kayanr <kayan.reid@gmail.com>
2021-02-14 14:38:19 -0500

    add /balance, /withdraw and /deposit routes
    add /balance, /withdraw and /deposit routes

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 }); 
 
+//Create a route `/balance` that gets the balance of the account
+app.get('/balance', (req, res) => {
+    res.send('this route gets the balance of the account')
+}); 
+
+app.get('/balance', (req, res) => {
+    res.send('this route gets the balance of the account')
+
+}); 
+
+//Create a route `/withdraw` that withdraws money from the account. The withdraw amount will be in the body
+
+app.get('/withdraw', (req, res) => {
+    res.send('this route gets the withdraws amount from the account')
+
+}); 
+
+//Create a route `/deposit` that deposits money from the account. The deposit amount will be in the body
+
+app.get('/deposit', (req, res) => {
+    res.send('this route deposits money from the account')
+
+}); 
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 });
